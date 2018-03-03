71dc708a35ec16512b2621ca685ae97595d8b0f7
iamsatyam <satyam@satyam.com>
2018-03-03 18:24:50 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,105 @@
+var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+var bodyParser =  require('body-parser');
+var bcrypt = require('bcryptjs');
+var jwt = require('jwt-simple');
+var app = express();
+
+var JWT_SECRET = 'catsmeow';
+
+var db = null;
+MongoClient.connect("mongodb://localhost:27017/", function(err, dbconn){
+	if(!err) {
+		console.log("we are connected");
+		var dbase = dbconn.db("mittens")
+		db = dbase;
+	}
+});
+
+app.use(bodyParser.json());
+
+app.use(express.static('public')); 
+
+app.get('/meows', function(req, res, next){
+
+	db.collection('meows', function(err, meowsCollection) {
+		meowsCollection.find().toArray(function(err, meows){
+			return res.json(meows);
+		});
+	});
+});
+
+app.post('/meows', function(req, res, next){
+
+		var token = req.headers.authorization;
+		var user = jwt.decode(token, JWT_SECRET);
+		
+		var newMeow = {
+			text: req.body.newMeow,
+			user: user._id,
+			username: user.username
+		};
+
+		db.collection("meows").insertOne(newMeow, function(err) {
+		    if (err) throw err;
+			return res.send();
+		 });
+	
+});
+
+app.put('/meows/remove', function(req, res, next){
+
+		var token = req.headers.authorization;
+		var user = jwt.decode(token, JWT_SECRET);
+		
+		var meowId = req.body.meow._id; 
+
+		db.collection("meows").deleteOne({_id: ObjectId(meowId), user: user._id}, function(err) {
+		     if (err) throw err;
+		 	return res.send();
+		  });
+	
+});
+
+app.post('/users', function(req, res, next){
+		
+		bcrypt.genSalt(10, function(err, salt){
+			bcrypt.hash(req.body.password, salt, function(err, hash){
+				var newUser = {
+					username: req.body.username,
+					password: hash
+				};
+
+				db.collection("users").insertOne(newUser, function(err) {
+				    if (err) throw err;
+					return res.send();
+				 });		
+			});
+		});
+
+		
+	
+});
+
+app.put('/users/signin', function(req, res, next){
+
+	db.collection('users', function(err, usersCollection){
+
+	usersCollection.findOne({username: req.body.username}, function(err, user){
+			
+		bcrypt.compare(req.body.password, user.password, function(err, result) {
+			if (result) {
+				var token = jwt.encode(user, JWT_SECRET);
+				return res.json({token: token});
+			}else {
+				return res.status(400).send();
+			}
+		});
+
+		});
+	});
+});
+app.listen(3000, function () {
+	console.log('Example app listening on port 3000');
+});
